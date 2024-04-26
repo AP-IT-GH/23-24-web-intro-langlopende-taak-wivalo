39f5187428aadbb5b23b8659c9923151bddd92f8
wivalo <wietse.vl@hotmail.com>
2024-04-26 21:54:15 +0200

    Full aanpassingen na controle docent.
    inline css verwijderd en in css file gestoken, javascript in aparte folder gestoken en pagina's opgekuist.

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+    // Function to show dropdown menu
+    function showDropdownMenu() {
+        var dropdownMenu = document.getElementById("dienstenDropdownMenu");
+        dropdownMenu.classList.add("show");
+    }
+
+    // Function to hide dropdown menu
+    function hideDropdownMenu() {
+        var dropdownMenu = document.getElementById("dienstenDropdownMenu");
+        dropdownMenu.classList.remove("show");
+    }
